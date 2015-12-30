1917a5ec7e6ccdb3e431659337450b49790e362a
Fabian Röpke <fabian.roepke@gmail.com>
2015-12-30 18:38:51 +0100

    landing-page html/css

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Typr</title>
+        <link href="./css/main.css" type="text/css" rel="stylesheet"/>
+        <link href="./css/landing-page.css" type="text/css" rel="stylesheet"/>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+        <div id="card">
+            <div id="square">
+                <div id="card-img">
+                    <div id="circle">
+                        <div id="card-head"></div>
+                        <div id="card-body"></div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="card-txt">
+                <h1 id="card-name">Max Mustermann</h1>
+                <h2 id="card-desc">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</h2>
+                <div id="card-links">    
+                    <a class="card-link" href="./pages/blog.php">Blog</a>
+                    <a class="card-link" href="./pages/portfolio.php">Portfolio</a>
+                </div>
+            </div>
+        </div>
+        
+        <div id="content">
+            <div class="article">
+                <h3 class="article-head">Lorem ipsum dolor</h3>
+                <hr>
+                <p class="article-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                <img class="article-img">
+            </div>
+            
+            <div class="article">
+                <h3 class="article-head">Temporibus autem quibusdam</h3>
+                <hr>
+                <p class="article-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                <img class="article-img">
+            </div>
+            
+            <div class="article">
+                <h3 class="article-head">Nemo enim ipsam</h3>
+                <hr>
+                <p class="article-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                <img class="article-img">
+            </div>
+            
+            <div class="article">
+                <h3 class="article-head">Nihil impedit quo minus id quod</h3>
+                <hr>
+                <p class="article-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                <img class="article-img">
+            </div>
+        </div>
+            
+            
+        </div>
+        
+        
+        <div id="footer">
+            <nav>
+                <ul id="navigation">
+                    <li><a href="./pages/blog.php" class="nav-link">Blog</a></li>
+                    <li><a href="./pages/portfolio.php" class="nav-link">Portfolio</a></li>
+                    <li><a href="./pages/contact.php" class="nav-link">Contact</a></li>
+                    <li><a href="./admin/landing.php" class="nav-link">Admin</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+	</body>
+</html>
