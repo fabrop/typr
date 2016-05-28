cf50a899a3efb62c669993cd12d8345610f11681
Fabian Röpke <fabian.roepke@gmail.com>
2016-05-29 01:37:57 +0200

    blog css

--- pages/blog.php
+++ pages/blog.php
@@ -39,32 +39,39 @@
         <div id="wrapper">
             
             <div id="content">
-                <div class="card"> 
-                    <h2 id="heading">Blog Posts</h2>
-                </div>
                 <article class="card article">
-                    <h3 class="article-heading"></h3>
-                    <p class="article-text"></p>
-                    <p class="date"></p>
+                    <h3 class="article-heading">Lorem ipsum dolor</h3>
+                    <p class="article-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                    <p class="date">21:33 - 29.05.2016</p>
                 </article>
                 <article class="card article">
-                    <h3 class="article-heading"></h3>
-                    <p class="article-text"></p>
-                    <p class="date"></p>
+                    <h3 class="article-heading">Nemo enim ipsam</h3>
+                    <p class="article-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+                    <p class="date">21:12 - 29.05.2016</p>
                 </article>
                 <article class="card article">
-                    <h3 class="article-heading"></h3>
-                    <p class="article-text"></p>
-                    <p class="date"></p>
+                    <h3 class="article-heading">Nihil impedit quo minus id quod</h3>
+                    <p class="article-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                    <p class="date">20:48 - 29.05.2016</p>
                 </article>
             </div>
             
             <div id="sidebar">
-                <div class="card">
-                
+                <div class="card" id="sidebar-tagcard">
+                    <h2 id="sidebar-tag">Alle Kategorien</h2>
                 </div>
-                <div class="card">
-                
+                <div class="card" id="sidebar-formcard">
+                    <p>Anzahl Posts:</p>
+                    <ul>
+                        <li><input type="checkbox">5</li>
+                        <li><input type="checkbox">10</li>
+                        <li><input type="checkbox">25</li>
+                    </ul>
+                    <p>Sortierung:</p>
+                    <ul>
+                        <li><input type=radio>Neueste zuerst</li>
+                        <li><input type=radio>&Auml;lteste zuerst</li>
+                    </ul>
                 </div>
             </div>
             
