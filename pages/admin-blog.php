61d9be74b3a48eba4c2abeede1634bd1dccee6b5
futzjug <achmed.lachnet@gmx.net>
2016-05-30 01:49:23 +0200

    admin blog

--- pages/admin-blog.php
+++ pages/admin-blog.php
@@ -1,34 +1,23 @@
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Typr-Admin</title>
+ <head>
+  <meta charset="UTF-8">
+  <title>Typr-Admin</title>
         <link href="../css/main.css" type="text/css" rel="stylesheet">
         <link href="../css/admin.css" type="text/css" rel="stylesheet">
         <link href="../css/admin-blog.css" type="text/css" rel="stylesheet">
         <link href="../img/favicon.png" type="image/png" rel="icon">
-	</head>
-	
-	<body>
+ </head>
+ 
+ <body>
         
-        <?php require '../modules/admin-navbar.php'; ?>    
+        <?php require '../modules/admin-navbar.php'; ?>  
+        <?php require_once '../modules/class-database.php'; ?> 
       
         <div class="section card" id="addpost">
             <a href="admin-blog-add.php">Post hinzufügen...</a>
         </div>
         
-        <?php
-            public function get_time($datetime){
-                time = substr($datetime, 11, 8);
-                return time;
-            }
-            public function get_date($datetime){
-                date = substr($datetime, 0, 9);
-                return date;
-            }
-            
-            
-        ?>
-        
+
         <div class="section card">
             <form action="" method="POST">
                 <table>
@@ -40,30 +29,38 @@
                         <th></th>
                         <th></th>
                     </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor</td>
-                        <td>21:09</td>
-                        <td>29.05.2016</td>
-                        <td>432 Wörter</td>
-                        <td><a href="#"><img alt="edit" src="../img/edit.png"></a></td>
-                        <td><img alt="edit" src="../img/delete.png"><input type="submit" name="deletePost" value="" /></td>
-                    </tr>
-                    <tr>
-                        <td>Temporibus autem quibusdam</td>
-                        <td>23:11</td>
-                        <td>17.03.2016</td>
-                        <td>127 Wörter</td>
-                        <td><a href="#"><img alt="edit" src="../img/edit.png"></a></td>
-                        <td><img alt="edit" src="../img/delete.png"><input type="submit" name="deletePost" value="" /></td>
-                    </tr>
-                    <tr>
-                        <td>Nihil impedit quo minus id quod</td>
-                        <td>11:08</td>
-                        <td>05.02.2015</td>
-                        <td>288 Wörter</td>
-                        <td><a href="#"><img alt="edit" src="../img/edit.png"></a></td>
-                        <td><img alt="edit" src="../img/delete.png"><input type="submit" name="deletePost" value="" /></td>
-                    </tr>
+                    <?php
+                        function get_time($datetime){
+                            $time = substr($datetime, 11, 8);
+                            return $time;
+                        }
+                        function get_date($datetime){
+                            $date = substr($datetime, 0, 10);
+                            return $date;
+                        }
+                    
+                        if(isset($_POST['deletePost'])) {
+                            $delete = $_POST['deletePost'];
+                            $query = "DELETE FROM Posts WHERE id = '$delete'"; 
+                            $db->delete($query);
+                        }
+
+                        require_once('../modules/class_query.php');
+                        $everything = $query->every_post();
+
+                        while ($row = mysqli_fetch_assoc($everything)){
+                            echo "<tr>\n";
+                                echo "<td><a href=\"blog-post.php?id=".$row['id']."\">".$row['title']."</a></td>";
+                                echo "<td>".get_time($row['date'])."</td>";
+                                echo "<td>".get_date($row['date'])."</td>";
+                                echo "<td>".str_word_count($row['content'])." Wörter</td>";
+                                echo "<td><a href=\"admin-blog-post.php?id=".$row['id']."\"><img alt=\"edit\" src=\"../img/edit.png\"></a></td>";
+                                echo "<td><img alt=\"edit\" src=\"../img/delete.png\"><input type=\"submit\" name=\"deletePost\" value=\"".$row['id']."\" /></td>";
+                            echo "</tr>\n";
+                        } 
+                        echo "</table>";
+                    ?>
+                    
                 </table>
             </form>
         </div>
