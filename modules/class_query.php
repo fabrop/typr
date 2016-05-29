81deb710bbaf5f6bea925a4f8b5b4ee743252585
futzjug <achmed.lachnet@gmx.net>
2016-05-30 00:24:42 +0200

    query

--- modules/class_query.php
+++ modules/class_query.php
@@ -6,7 +6,21 @@
 			public function every_post(){
 				global $db;
 				$query = "
-							SELECT * FROM posts
+							SELECT * 
+							FROM posts
+						";
+						
+				return $db->select($query);
+			}
+			
+			public function get_posts($nbr,$page,$sort){
+				global $db;
+				$start = ($nbr * ($page - 1)) + 1;
+				$query = "
+							SELECT * 
+							FROM posts
+							ORDER BY id $sort
+							LIMIT $start,$nbr
 						";
 						
 				return $db->select($query);
