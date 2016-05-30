a5861244408a15be094e8722e95299552badab31
Fabian Röpke <fabian.roepke@gmail.com>
2016-05-30 21:36:41 +0200

    safety first

--- pages/blog.php
+++ pages/blog.php
@@ -40,7 +40,7 @@
 					$raw_number = $query->get_number_posts();
 					$number_arr = mysqli_fetch_assoc($raw_number);
 					$number = $number_arr['count_posts'];
-					//echo $number;
+                    
 					$cnt_pages = ceil($number / $nbr_posts);
 					if(isset($_GET['page'])){
 						$cur_page = $_GET['page'];
@@ -73,42 +73,59 @@
 								</a>
 							</article>';
 					}
-				?>
-                </div>
-
-                <div id="sidebar" class="card"> 
-                    <form method="GET">
-                        <p>Kategorien:</p>
-                        <ul>
-                            <li><input type="checkbox" name="tag" onChange="this.form.submit();" value="1" id="tag1" checked><label for="tag1">Tag 1</label></li>
+				
+                echo '</div>';
+                
+                echo '
+                    <div id="sidebar" class="card"> 
+                        <form method="GET">
+                            <p>Kategorien:</p>
+                            <ul>
+                            ';
+                            
+                            /*
+                            <li><input type="checkbox" name="tag" onChange="this.form.submit();" value="1" id="tag1" ''><label for="tag1">Tag 1</label></li>
                             <li><input type="checkbox" name="tag" onChange="this.form.submit();" value="2" id="tag2" checked><label for="tag2">Tag 2</label></li>
                             <li><input type="checkbox" name="tag" onChange="this.form.submit();" value="3" id="tag3" checked><label for="tag3">Tag 3</label></li>
-                        </ul>
-                        <hr>
-                        <p>Anzahl Posts:</p>
-                        <ul>
+                            */
+                            echo '
+                            </ul>
+                            <hr>
+                            <p>Anzahl Posts:</p>
+                            <ul>
+                            ';
+                            /* 
                             <li><input type="radio" name="number" onChange="this.form.submit();" value="6" id="number6" checked><label for="number6">6</label></li>
                             <li><input type="radio" name="number" onChange="this.form.submit();" value="12" id="number12"><label for="number12">12</label></li>
                             <li><input type="radio" name="number" onChange="this.form.submit();" value="20" id="number20"><label for="number20">20</label></li>
-                        </ul>
-                        <hr>
-                        <p>Sortierung:</p>
-                        <ul>
+                            */
+                            echo '
+                            </ul>
+                            <hr>
+                            <p>Sortierung:</p>
+                            <ul>
+                            ';
+                            /*
                             <li><input type=radio name="sort" onChange="this.form.submit();" value="DESC" id="sort-new" checked><label for="sort-new">Neueste zuerst</label></li>
                             <li><input type=radio name="sort" onChange="this.form.submit();" value="ASC" id="sort-old"><label for="sort-old">&Auml;lteste zuerst</label></li>
-                        </ul>
-                        <hr>
-                        <p>
-                            <a href="admin-blog.php">Posts verwalten...</a>
-                            <br>
-                            <a href="admin-blog-add.php">Post schreiben...</a>
-                        </p>
-                    </form>
+                            */
+                            echo '
+                            </ul>
+                            <hr>
+                            <p>
+                                <a href="admin-blog.php">Posts verwalten...</a>
+                                <br>
+                                <a href="admin-blog-add.php">Post schreiben...</a>
+                            </p>
+                        </form>
+                    </div>
+                '; 
+                    
+                ?>
+                    
                 </div>
-
-            </div>
-        </main>
-        
+            </main>
+               
         <nav id="pagination">
             <ul>
 			<form action="" method="GET">
