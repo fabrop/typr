a71726c7db167df4b38036e2119b913097def10c
Fabian Röpke <fabian.roepke@gmail.com>
2016-05-30 20:40:17 +0200

    contact

---
+++ pages/contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Kontakt</title>
+        <link href="../css/main.css" type="text/css" rel="stylesheet">
+        <link href="../css/contact.css" type="text/css" rel="stylesheet">
+        <link href="../css/navbar.css" type="text/css" rel="stylesheet">
+        <link href="../img/favicon.png" type="image/png" rel="icon">
+	</head>
+	
+	<body>
+        <main>
+            <?php 
+                require_once '../modules/navbar.php';
+            ?>
+
+            <div id="wrapper" class="card">
+                <h3>Name</h3>
+                <p>Max Mustermann</p>
+                
+                <h3>Telefonnummer</h3>
+                <p>01235 12345</p>
+                
+                <h3>Email-Adresse</h3>
+                <p>max.mustermann@provider.de</p>
+                
+                <h3>Adresse</h3>
+                <p>Hauptstraße 123<br>12345 Musterstadt</p>
+            </div>
+	</body>
+</html>
