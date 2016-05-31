9f680658464e9760b0ba66116b147d2a32e3e183
Fabian Röpke <fabian.roepke@gmail.com>
2016-05-31 05:50:46 +0200

    login css

--- admin/login.php
+++ admin/login.php
@@ -1,17 +1,23 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+        <link href="../css/main.css" type="text/css" rel="stylesheet">
+        <link href="../css/admin.css" type="text/css" rel="stylesheet">
+        <link href="../css/login.css" type="text/css" rel="stylesheet">
+        <link href="../img/favicon.png" type="image/png" rel="icon">
 	</head>
 	
 	<body>
-		<form action="validate.php" method="post">
-			<label for="userid">ID</label>
+		<form action="validate.php" method="post" class="card">
+			<label for="userid">User-ID</label>
 			<input type="text" name="userid" id="userid" />
 			<br />
 			<label for="password">Password</label>
 			<input type="password" name="password" id="password" />
 			<br />
-			<input type="submit" name="submit" value="Submit" />
+			<input type="submit" name="submit" value="Submit" id="save"/>
 		</form>
 	</body>
 </html>
